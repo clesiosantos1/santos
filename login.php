a024076755117e9c7f16c7b695281aec872ac97e
clesio santos <clesio.santos@Jupiter.co.ao>
2022-09-05 11:17:22 +0100

    alterado

--- login.php
+++ login.php
@@ -3,9 +3,13 @@
 $nome = "Lar-caridade123";
 $senha = "Rosa";
 
-if($nome == "Lar-caridade123"){
-    
+if($nome == "Lar-caridade123" || $senha == "Rosa"){
+    echo "aproveite";
+}else{
+    echo "erro";
 }
+    
+
 
 
 
