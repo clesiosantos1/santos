c1a284bd2037ab2de64f3d4ebfa70c34d53b2126
clesio santos <clesio.santos@Jupiter.co.ao>
2022-09-06 14:03:53 +0100

    ola

--- administrativo.php
+++ administrativo.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once("config.php");
+
 session_start();
 if(!empty($_SESSION['id'])){
 	echo "Olá ".$_SESSION['nome'].", Bem vindo <br>";
@@ -7,4 +10,70 @@ if(!empty($_SESSION['id'])){
 	$_SESSION['msg'] = "Área restrita";
 	header("Location: login.php");	
 }
+
+$sql = "SELECT * FROM usuarios ORDER BY id DESC";
+
+$result = $conexao->query($sql);
+
+print_r($result);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="bulma.min.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+</head>
+<body>
+	
+
+<style>
+	
+	.container{
+		display:flex;
+		flex-wrap: wrap;
+		justify-content:center;
+	}
+
+
+</style>
+
+<div class="container">
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Apelido</th>
+      <th scope="col">Endereco</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Telefone</th>
+      <th scope="col">Assunto</th>
+      <th scope="col">Mensagem</th>
+      <th scope="col">...</th>
+      <th scope="col">...</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+  </tbody>
+</table>
+</div>
+
+
+
+
+</script>
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
