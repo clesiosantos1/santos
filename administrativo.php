d2d8ba8d395b2eb0e9f9d3df7905f8a8377c7e2d
clesio santos <clesio.santos@Jupiter.co.ao>
2022-09-12 17:00:33 +0100

    alteracao do texto

--- administrativo.php
+++ administrativo.php
@@ -1,3 +1,7 @@
+
+
+
+
 <div  class="containere">
 
 <?php
@@ -102,9 +106,6 @@ $result = $conexao->query($sql);
       echo "<td> <a class='btn btn-danger btn-sm' href=''>  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
       <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
     </svg></td>";
-    
-
-
       echo "</tr>";
     }
     ?>
@@ -120,3 +121,7 @@ $result = $conexao->query($sql);
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
+
+
